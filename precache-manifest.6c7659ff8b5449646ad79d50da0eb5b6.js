2bf61cfada20a96efb55fa947540dbf9450dc00b
menyawino <omarahmedd@aucegypt.edu>
2025-01-17 18:18:39 +0200

    Updates

---
+++ precache-manifest.6c7659ff8b5449646ad79d50da0eb5b6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6322f7b26278afd8d195715ca84e8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "807233032ae65e45ab2d",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a0437bb37d7008ffff1d",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "e91f38cf778101f6ca34",
+    "url": "/portfolio/static/css/main.dbea25f8.chunk.css"
+  },
+  {
+    "revision": "11a8fed93a7e41a2851a",
+    "url": "/portfolio/static/js/2.d09ecd50.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/portfolio/static/js/2.d09ecd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807233032ae65e45ab2d",
+    "url": "/portfolio/static/js/3.1deb9f6d.chunk.js"
+  },
+  {
+    "revision": "a0437bb37d7008ffff1d",
+    "url": "/portfolio/static/js/4.ae934b66.chunk.js"
+  },
+  {
+    "revision": "e91f38cf778101f6ca34",
+    "url": "/portfolio/static/js/main.65bd99cb.chunk.js"
+  },
+  {
+    "revision": "e11eca7dc6eb9006a2a4",
+    "url": "/portfolio/static/js/runtime-main.9053660c.js"
+  },
+  {
+    "revision": "a54bd3fef9dc7e74bfb85b800c629943",
+    "url": "/portfolio/static/media/3ashLogo.a54bd3fe.jpg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "baf0e528ca199903bba9551f44c774c1",
+    "url": "/portfolio/static/media/Omar_Ahmed_Resume.baf0e528.pdf"
+  },
+  {
+    "revision": "2e217e22fec6a2d3f69acb7a89916328",
+    "url": "/portfolio/static/media/aucLogo.2e217e22.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "fe618004fecb172e2837e73392f6579c",
+    "url": "/portfolio/static/media/garnLogo.fe618004.jpg"
+  },
+  {
+    "revision": "64c8ac8de48ebf76158b06b8e18d590f",
+    "url": "/portfolio/static/media/hhLogo.64c8ac8d.jpg"
+  },
+  {
+    "revision": "083c663f462e1c83669e4dfc502413fc",
+    "url": "/portfolio/static/media/icanLogo.083c663f.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "89f215d31f26b9312e3c6e4f9dfaa433",
+    "url": "/portfolio/static/media/supertumsLogo.89f215d3.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
